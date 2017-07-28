4fb61060684d4737be60f847b806fca343b8885a
Florent Solt <florent@solt.biz>
2017-07-28 14:53:17 +0200

    simplify

--- test/thumb.js
+++ test/thumb.js
@@ -6,10 +6,9 @@ var Promise = require("bluebird"),
     colors = require('colors'),
     path = require('path'),
     fs = require('fs'),
-    winston = require('winston');
-    // sharp = require('sharp');
-
-var spawn = require('./spawn');
+    winston = require('winston'),
+    util = require('util'),
+    execFile = util.promisify(require('child_process').execFile);
 
 Promise.promisifyAll(fs);
 
@@ -17,7 +16,6 @@ program
   .version('0.4.2')
   .option('-a, --album [name]', 'specify an album')
   .option('-p, --path [directory]', 'specify the directory where albums are')
-  .option('-c, --concurrency [limit]', 'specify a concurrency limit', parseInt, 3)
   .option('-d, --debug', 'output debug information')
   .parse(process.argv);
 
@@ -36,37 +34,45 @@ var directory = program.path ?
   path.join(__dirname, '..', 'albums', program.album);
 
 fs.readdirAsync(directory)
-  .map(function(filename) {
+  .each(function(filename) {
     var promises = [],
         output;
 
     if (filename.toLowerCase().match(/\.jpg$/)) {
       winston.debug(filename);
       output = path.join(__dirname, 'cache', 'preview-' + filename);
-      // promises.push(sharp(path.join(directory, filename))
-      //   .resize(2560, 1600)
-      //   .jpeg({quality: 85, progressive: true})
-      //   .toFile(output)
-      // );
-      promises.push(spawn('vipsthumbnail', [
+
+      promises.push(execFile('vipsthumbnail', [
         '-s', '2560x1600',
         path.join(directory, filename),
         '-o', output + '[Q=85]'
-      ]));
+      ]).then(function() {
+        return execFile('jpegtran', [
+          '-optimize',
+          '-copy', 'none',
+          '-progressive',
+          '-outfile', output,
+          output
+        ]);
+      }));
 
-      // output = path.join(__dirname, 'cache', 'thumb-' + filename);
-      // promises.push(sharp(path.join(directory, filename))
-      //   .resize(null, 300)
-      //   .jpeg({quality: 85, progressive: true})
-      //   .toFile(output)
-      // );
+      output = path.join(__dirname, 'cache', 'thumb-' + filename);
 
-      promises.push(spawn('vipsthumbnail', [
+      promises.push(execFile('vipsthumbnail', [
         '-s', 'x300',
         path.join(directory, filename),
         '-o', output + '[Q=85]'
-      ]));
+      ]).then(function() {
+        return execFile('jpegtran', [
+          '-optimize',
+          '-copy', 'none',
+          '-progressive',
+          '-outfile', output,
+          output
+        ]);
+      }));
+
     }
 
     return Promise.all(promises);
-  }, {concurrency: program.concurrency});
+  });
